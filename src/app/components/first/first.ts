f21234a7dd66612f7699ed80ac2d318a7b6fc298
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-23 12:25:52 +0100

    exemple binding

--- src/app/components/first/first.ts
+++ src/app/components/first/first.ts
@@ -8,5 +8,28 @@ import { Second } from "../second/second";
   styleUrl: './first.css',
 })
 export class First {
+  // attributs => Etat / State
+  name = 'aymen';
+  // méthodes => Comportement / Beahviour
+  /**
+   * @var gére le fait que l'élement est caché ou non
+   */
+  isHidden = false;
+  /**
+   * @var le contenu de l'input
+   */
+  message = '';
+  constructor() {
+    // setTimeout(() => {
+    //   this.name = "cherif"
+    // }, 2000)
+  }
 
+  showHide() {
+    this.isHidden = !this.isHidden;
+  }
+
+  changeMessage(newMessage: string) {
+    this.message = newMessage;
+  }
 }
