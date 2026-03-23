65f320cb602dc26713a8bfd020636fd7c9f038b5
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-23 13:58:58 +0100

    exemple two Way Data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { First } from "./components/first/first";
 import { Second } from "./components/second/second";
 import { Color } from "./components/color/color";
+import { Two } from "./components/two/two";
 
 @Component({
   selector: 'app-root',
-  imports: [First, Second, Color],
+  imports: [First, Second, Color, Two],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
