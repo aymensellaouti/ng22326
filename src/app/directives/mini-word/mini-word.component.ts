f54bf475c4424b588a91c27e7bccbb66fc3549b1
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-24 14:34:36 +0100

    feat: enhance MiniWord correctoin mini Word component with reactive properties for color, font, and size

--- src/app/directives/mini-word/mini-word.component.ts
+++ src/app/directives/mini-word/mini-word.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mini-word',
   templateUrl: './mini-word.component.html',
-  styleUrls: ['./mini-word.component.css']
+  styleUrls: ['./mini-word.component.css'],
+  imports: [FormsModule]
 })
 export class MiniWordComponent {
-
+  // Définition state
+  color = signal('#0b354a');
+  font = signal('garamond');
+  size = signal(75);
 }
