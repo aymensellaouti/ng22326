7a51e2d87c90bc252db321232dd623ac86ab5077
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-24 14:09:31 +0100

    feat: add MiniWord and NgStyle components with templates and styles exemple style et ngStyle et start mini word exo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 
 import { CvPage } from "./cv/cv-page/cv-page";
+import { NgStyleComponet } from "./directives/ng-style/ng-style";
+import { CvItem } from "./cv/cv-item/cv-item";
+import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CvPage],
+  imports: [CvPage, NgStyleComponet, CvItem, MiniWordComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
