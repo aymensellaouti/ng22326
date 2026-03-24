8d6832db162c30724b50d74e75bef15f6daa6f2d
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-24 09:55:41 +0100

    exemple et start exercice Computed feat: implement TTC calculator and update color component with signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { Component, signal } from '@angular/core';
 import { Card } from './components/card/card';
 import { Counter } from "./signal/counter/counter";
 import { Color } from "./components/color/color";
+import { Som } from "./signal/som/som";
+import { TtcComponent } from "./signal/ttc/ttc.component";
 
 @Component({
   selector: 'app-root',
-  imports: [Color],
+  imports: [Color, Som, TtcComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
