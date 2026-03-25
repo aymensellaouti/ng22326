de28222e83622918028ddd4fee5e92db9b0c9a21
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-25 14:19:45 +0100

    router acr et start detailsCv exo

--- src/app/components/second/second.ts
+++ src/app/components/second/second.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-second',
   imports: [],
-  template: `<h2>Second Component</h2>`,
+  template: `<h2>Hello {{ name() }}</h2>`,
   styles: ``,
 })
 export class Second {
-
+  name = signal('');
+  acr = inject(ActivatedRoute);
+  router = inject(Router);
+  redirectToCv() {
+    this.router.navigate(['cv']);
+  }
+  constructor() {
+    this.name.set(this.acr.snapshot.params['name'])
+  }
 }
