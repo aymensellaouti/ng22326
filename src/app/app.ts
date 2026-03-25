a23dd2a73479af969063573db132bed52d7c7144
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-25 14:06:17 +0100

    mise en place du routeur + navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,12 @@ import { CvItem } from "./cv/cv-item/cv-item";
 import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
 import { TtcComponent } from "./signal/ttc/ttc.component";
 import { TodoComponent } from "./todo/todo/todo.component";
+import { RouterOutlet } from "@angular/router";
+import { Navbar } from "./components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [CvPage, NgStyleComponet, CvItem, MiniWordComponent, TtcComponent, TodoComponent],
+  imports: [CvPage, NgStyleComponet, CvItem, MiniWordComponent, TtcComponent, TodoComponent, RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
