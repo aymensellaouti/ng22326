fcf59c9310ef547b3e08948104cd1f5f9d436c9f
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-26 12:01:34 +0100

    add api endpoints

--- src/app/config/app-api.config.ts
+++ src/app/config/app-api.config.ts
@@ -1,3 +1,5 @@
 export const APP_API = {
   todo: 'https://jsonplaceholder.typicode.com/todos',
+  docuCvApi: 'https://apilb.tridevs.net/explorer/#/personne',
+  cv: 'https://apilb.tridevs.net/api/personnes/',
 };
